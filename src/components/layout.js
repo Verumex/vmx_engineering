b040164bd1d7b0d3b87428f00b222aa647e295ac
Jan Klimo <jan.klimo@gmail.com>
2019-11-15 14:31:08 +0700

    Move layout styles from JS to CSS to avoid initial flash of unstyled content

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,59 +1,33 @@
 import React from "react";
-import styled from "react-emotion";
 import { MDXProvider } from "@mdx-js/react";
 import ThemeProvider from "./themeProvider";
 import mdxComponents from "./mdxComponents";
 import Sidebar from "./sidebar";
 import RightSidebar from "./rightSidebar";
 
-const Wrapper = styled('div')`
-  display: flex;
-  justify-content: space-between;
+const Wrapper = ({ children }) => (
+  <div className="body-wrapper">{children}</div>
+);
 
-  @media only screen and (max-width: 767px) {
-    display: block;
-  }
-`;
+const Content = ({ children }) => (
+  <main className="content-wrapper">{children}</main>
+);
 
-const Content = styled('main')`
-  display: flex;
-  flex-grow: 1;
-  margin: 0px 88px;
-  margin-top: 3rem;
+const MaxWidth = ({ children }) => <div className="max-width">{children}</div>;
 
-  @media only screen and (max-width: 1023px) {
-    padding-left: 0;
-    margin: 0 10px;
-    margin-top: 3rem;
-  }
-`;
-
-const MaxWidth = styled('div')`
-
-  @media only screen and (max-width: 50rem) {
-    width: 100%;
-    position: relative;
-  }
-`;
-const LeftSideBarWidth = styled('div')`
-  width: 298px;
-`;
-const RightSideBarWidth = styled('div')`
-  width: 224px;
-`;
 const Layout = ({ children, location }) => (
   <ThemeProvider location={location}>
     <MDXProvider components={mdxComponents}>
       <Wrapper>
-        <LeftSideBarWidth className={'hidden-xs'}>
+        <div className="hidden-xs">
           <Sidebar location={location} />
-        </LeftSideBarWidth>
+        </div>
         <Content>
           <MaxWidth>{children}</MaxWidth>
         </Content>
-        <RightSideBarWidth className={'hidden-xs'}>
+        <div className="hidden-xs">
           <RightSidebar location={location} />
-        </RightSideBarWidth>
+        </div>
       </Wrapper>
     </MDXProvider>
   </ThemeProvider>
