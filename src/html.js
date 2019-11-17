9b1e36c8c2314007e3f34241aa42dfde28b23173
Jan Klimo <jan.klimo@gmail.com>
2019-11-17 14:34:04 +0700

    Fix font preloading

--- src/html.js
+++ src/html.js
@@ -1,10 +1,10 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
 import config from "../config";
 
 export default class HTML extends React.Component {
   render() {
-    const favicon = require('./components/images/favicon-32x32.png');
+    const favicon = require("./components/images/favicon-32x32.png");
     return (
       <html {...this.props.htmlAttributes}>
         <head>
@@ -14,23 +14,38 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          {config.siteMetadata.ogImage ? 
-            (<meta property="og:image" content={config.siteMetadata.ogImage} />) : null
-          }
+          {config.siteMetadata.ogImage ? (
+            <meta property="og:image" content={config.siteMetadata.ogImage} />
+          ) : null}
           <meta property="twitter:card" content="summary_large_image" />
-          {config.siteMetadata.ogImage ? 
-            (<meta property="twitter:image" content={config.siteMetadata.ogImage} />) : null
-          }
+          {config.siteMetadata.ogImage ? (
+            <meta
+              property="twitter:image"
+              content={config.siteMetadata.ogImage}
+            />
+          ) : null}
           <link rel="shortcut icon" type="image/svg" href={favicon} />
-          <link rel="preload" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" as="style">
-          <link rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous" />
+          <link
+            rel="preload"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700"
+            as="style"
+          />
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+            integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+            crossOrigin="anonymous"
+          />
           <script
-  			  src="https://code.jquery.com/jquery-3.3.1.min.js"
-  			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  			  crossOrigin="anonymous"></script>
-          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossOrigin="anonymous"></script>
+            src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossOrigin="anonymous"
+          ></script>
+          <script
+            src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+            crossOrigin="anonymous"
+          ></script>
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
@@ -42,19 +57,19 @@ export default class HTML extends React.Component {
           />
           {this.props.postBodyComponents}
           <script
-          dangerouslySetInnerHTML={{
-            __html: `
+            dangerouslySetInnerHTML={{
+              __html: `
             $(document).on('click','.navbar-collapse.in',function(e) {
               if( $(e.target).is('a') ) {
                 $(this).collapse('hide');
               }
             });
-            `
-          }}
+            `,
+            }}
           />
         </body>
       </html>
-    )
+    );
   }
 }
 
@@ -65,4 +80,4 @@ HTML.propTypes = {
   preBodyComponents: PropTypes.array,
   body: PropTypes.string,
   postBodyComponents: PropTypes.array,
-}
+};
