22764868e97fc42df257e153e3291dab25954c89
Jan Klimo <jan.klimo@gmail.com>
2020-09-11 21:35:14 +0700

    Add noindex meta tag
    This removes pages from search results regardless if they have sites
    linking to them. See https://youtu.be/KBdEwpRQRD0 for more.

--- src/html.js
+++ src/html.js
@@ -9,6 +9,7 @@ export default class HTML extends React.Component {
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta name="robots" content="noindex" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
